6906e8bb6478aba053e8ac7d4a803428cb9c2adc
Sujan Shrestha <shrsujan2007@gmail.com>
2018-09-12 21:41:02 +0545

    add medal spark
    - add spark to medal
    - add logic to contain spark inside the medal
    - add logic to randomize spark position on end of animation

--- scripts/game.gd
+++ scripts/game.gd
@@ -6,6 +6,11 @@ const GROUP_PIPES = "pipes"
 const GROUP_GROUNDS = "grounds"
 const GROUP_BIRDS = "birds"
 
+const MEDAL_BRONZE = 10
+const MEDAL_SILVER = 20
+const MEDAL_GOLD = 30
+const MEDAL_PLATINUM = 40
+
 var score_best = 0 setget _set_score_best
 var score_current = 0 setget _set_score_current
 
